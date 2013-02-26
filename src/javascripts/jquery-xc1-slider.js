5558bc242cc75ed5a71f43fbfbd2ce5b4a6b5af1
ParHaggstrom <mail@parhaggstrom.com>
2013-02-26 16:08:27 +0100

    began workin on the "new" slider

--- src/javascripts/jquery-xc1-slider.js
+++ src/javascripts/jquery-xc1-slider.js
@@ -109,5 +109,166 @@
 	};
 })(jQuery);
 
+$(window).bind('resize', function() {
+	$.doTimeout( 'resize', 250, function(){
+		xc1InfiniteSlider(); 
+	});
+});
+
+$(window).load(function() {
+	xc1InfiniteSlider();	
+});
+
+
+function xc1InfiniteSlider() {
+	var infiniteSlider = new Array();
+	
+	$('.infinite-slides').each(function(index, item) {
+		xc1InfiniteSliderImages($(this));
+	});
+}
+
+function xc1InfiniteSliderImages(infiniteSlider) {
+	var sectionHeight = infiniteSlider.height();	
+
+	$('li img', infiniteSlider).each(function(index, item) {
+		var newImage = new Image();
+		newImage.src = $(this).attr('src');
+		var newWidth = Math.floor( newImage.width * (sectionHeight / newImage.height) );
+		var newHeight = sectionHeight;
+		
+		$(this).attr('width', newWidth).attr('height', newHeight).parent().css({'width' : newWidth + 'px', 'height' : newHeight + 'px'});
+					
+		if(index == $('li', infiniteSlider).length-1) {
+			if(infiniteSlider.data('auto') == 'auto') { var infiniteAuto = true } else { var infiniteAuto = false }
+			if(infiniteSlider.data('speed') == '') { var infiniteSpeed = 70 } else { var infiniteSpeed = infiniteSlider.data('speed'); }	
+			xc1InfiniteSliderInit(infiniteSlider, infiniteAuto, infiniteSpeed);
+		}
+	});
+}
+
+
+function xc1InfiniteSliderInit(infiniteSlider, infiniteAuto, infiniteSpeed) {
+		
+	var infiniteSliderContainer = infiniteSlider.parent();
+	var infiniteSliderItems = infiniteSlider.children();
+	var infiniteSliderLoaded = infiniteSlider.data('loaded');
+	
+	var infiniteVarWidth = 0;
+	var infiniteVarPos = 0;
+	var infiniteVarPosMin = 0;
+	var infiniteVarPosMax = 0;
+	var infiniteVarInterval;
+	var infiniteVarDirection = 'forward';
+	
+	
+	// IE Fixx
+	var infiniteVarIE = false;
+	if($('html').hasClass('ie')) { infiniteVarIE = true; }
+	
+	if(!infiniteSliderLoaded) {
+	
+		// Navigation
+		//var infiniteVarPause = $('<div class="infinite-scroll-pause"></div>');
+		var infiniteVarForward = $('<div class="infinite-scroll-forward"></div>');
+		var infiniteVarBackward = $('<div class="infinite-scroll-backward"></div>');
+		var infiniteVarPause = infiniteSliderItems;
+		
+		// Append navigation
+		//infiniteSliderContainer.append(infiniteVarPause);
+		infiniteSliderContainer.append(infiniteVarForward);
+		infiniteSliderContainer.append(infiniteVarBackward);
+		
+		// Bind the Navigation
+		infiniteVarPause.on('mouseover', function() { infinitePause(); }).on('mouseout', function() { infiniteResume(); });
+		infiniteVarForward.on('mouseover', function() { infiniteForward(4); }).on('mouseout', function() { infiniteResume(); });
+		infiniteVarBackward.on('mouseover', function() { infiniteBackward(4); }).on('mouseout', function() { infiniteResume(); });
+		
+		// Append extra content
+		var content = new Array();
+		
+		content.push(infiniteSliderItems.slice().clone(true));
+		content.push(infiniteSliderItems.slice().clone(true));
+		content.push(infiniteSliderItems.slice().clone(true));
+		content.push(infiniteSliderItems.slice().clone(true));
+		content.push(infiniteSliderItems.slice().clone(true));
+		
+		for(var i = 0; i < content.length; i++) {
+			infiniteSlider.append(content[i]);
+		}
+	
+	}
+	
+	// Set sizes
+	infiniteSliderItems.each(function() {
+		infiniteVarWidth = Math.round(infiniteVarWidth + $(this).width());
+	});
+	infiniteSlider.css({'width' : infiniteVarWidth*6 + 'px'});
+	
+	// Set Positions
+	infiniteVarPosMax = infiniteVarWidth*4;
+	infiniteVarPosMin = infiniteVarWidth*2;
+	
+	// Start the slider on init
+	if(infiniteAuto == true && !infiniteSliderLoaded) {
+		infiniteForward(1);
+	}
+		
+	// Set the slider as loaded
+	infiniteSlider.data('loaded', 'true');
+	
+	// Functions
+	function infiniteForward(speed) {
+		clearInterval(infiniteVarInterval);
+		infiniteVarInterval = setInterval(function() {
+			if(!infiniteVarIE) {
+				infiniteSlider.attr('style', 'width: ' + infiniteVarWidth*6 + 'px; -moz-transform: translate3d(-' + infiniteVarPos +'px, 0, 0); -webkit-transform:translate3d(-' + infiniteVarPos +'px,0,0); transform:translate3d(-' + infiniteVarPos +'px,0,0);');
+			} else {
+				infiniteSlider.css({'left' : '-' + infiniteVarPos + 'px'});
+			}
+			
+			if (infiniteVarPos >= infiniteVarPosMin && infiniteVarPos <= infiniteVarPosMax) {
+				infiniteVarPos++;
+			} else {
+				infiniteVarPos = infiniteVarPosMin;
+			}
+			infiniteVarDirection = 'forward';
+		}, infiniteSpeed/speed);
+	}
+
+	function infiniteBackward(speed) {
+		clearInterval(infiniteVarInterval);
+		infiniteVarInterval = setInterval(function() {
+			if(!infiniteVarIE) {
+				infiniteSlider.attr('style', 'width: ' + infiniteVarWidth*6 + 'px; -moz-transform: translate3d(-' + infiniteVarPos +'px, 0, 0); -webkit-transform:translate3d(-' + infiniteVarPos +'px,0,0); transform:translate3d(-' + infiniteVarPos +'px,0,0);');
+			} else {
+				infiniteSlider.css({'left' : '-' + infiniteVarPos + 'px'});
+			}
+			
+			if (infiniteVarPos >= infiniteVarPosMin && infiniteVarPos <= infiniteVarPosMax) {
+				infiniteVarPos--;
+			} else {
+				infiniteVarPos = infiniteVarPosMax;
+			}
+			infiniteVarDirection = 'backward';
+		}, infiniteSpeed/speed);
+	}
+	
+	function infinitePause() {
+		clearInterval(infiniteVarInterval);
+	}
+
+	function infiniteResume() {
+		if(infiniteAuto == true) {
+			if(infiniteVarDirection == 'forward') {
+				infiniteForward(1);		
+			} else {
+				infiniteBackward(1);
+			}
+		}
+	}
+	
+
+}
 
 
