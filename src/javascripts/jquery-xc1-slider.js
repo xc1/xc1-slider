e2889e9697183a50bb6b7b90adc39b684aafc31b
ParHaggstrom <mail@parhaggstrom.com>
2013-02-27 10:53:16 +0100

    Base done

--- src/javascripts/jquery-xc1-slider.js
+++ src/javascripts/jquery-xc1-slider.js
@@ -1,30 +1,112 @@
 (function($){
-	$.fn.xc1Slider = function () {
+
+  // xc1Slider
+  $.xc1Slider = function(slider, options) {
+	alert('We got a winner!');
+	
+	slider.vars = $.extend({}, $.xc1Slider.defaults, options);
+	slider.load = {
+		varWidth: 0,
+		varHeight: 0,
+		varPos: 0,
+		varPosMin: 0,
+		varPosMax: 0,
+		varInterval: ''
+	};
 	
 	/* Vars */
-	var infiniteSliderContainer = infiniteSlider.parent();
+	
+/*
+	var xc1SliderContainer = infiniteSlider.parent();
 	var infiniteSliderItems = infiniteSlider.children();
 	var infiniteSliderLoaded = infiniteSlider.data('loaded');
 	
-	var infiniteVarWidth = 0;
-	var infiniteVarPos = 0;
-	var infiniteVarPosMin = 0;
-	var infiniteVarPosMax = 0;
-	var infiniteVarInterval;
-	var infiniteVarDirection = 'forward';
-	
-	
+*/
 	
 	// IE Fixx
 	var infiniteVarIE = false;
-	if($('html').hasClass('ie')) { infiniteVarIE = true; }	
-
+	if($('html').hasClass('ie')) { sliderVarIE = true; }	
+	
+	/* Functions */
+	
+	load();
+	
 	// Load everything first time
 	function load() {
+		//Wrap the slider in 
+		slider.find('ul').wrap('<div class="slider-container" />');		
+		// Append the manual nav
+		slider.append('<div class="slider-nav-container"><ul class="slider-nav-pagination"></ul><ul class="slider-nav-direction"><li class="slider-nav-left"><a href="#" title="Previous">&lt;</a></li><li class="slider-nav-right"><a href="#" title="Next">&gt;</a></li></ul></div>');
+		
+
+		
+		// Set the slider as loaded
+		slider.data('loaded', 'true');
+	}
+
+	// Navigations
+	function nav() {
 		
 	}
 
+
+
 	
+	// Functions
+	function forward(speed) {
+		clearInterval(infiniteVarInterval);
+		infiniteVarInterval = setInterval(function() {
+			if(!infiniteVarIE) {
+				infiniteSlider.attr('style', 'width: ' + infiniteVarWidth*6 + 'px; -moz-transform: translate3d(-' + infiniteVarPos +'px, 0, 0); -webkit-transform:translate3d(-' + infiniteVarPos +'px,0,0); transform:translate3d(-' + infiniteVarPos +'px,0,0);');
+			} else {
+				infiniteSlider.css({'left' : '-' + infiniteVarPos + 'px'});
+			}
+			
+			if (infiniteVarPos >= infiniteVarPosMin && infiniteVarPos <= infiniteVarPosMax) {
+				infiniteVarPos++;
+			} else {
+				infiniteVarPos = infiniteVarPosMin;
+			}
+			infiniteVarDirection = 'forward';
+		}, infiniteSpeed/speed);
+	}
+
+	function backward(speed) {
+		clearInterval(infiniteVarInterval);
+		infiniteVarInterval = setInterval(function() {
+			if(!infiniteVarIE) {
+				infiniteSlider.attr('style', 'width: ' + infiniteVarWidth*6 + 'px; -moz-transform: translate3d(-' + infiniteVarPos +'px, 0, 0); -webkit-transform:translate3d(-' + infiniteVarPos +'px,0,0); transform:translate3d(-' + infiniteVarPos +'px,0,0);');
+			} else {
+				infiniteSlider.css({'left' : '-' + infiniteVarPos + 'px'});
+			}
+			
+			if (infiniteVarPos >= infiniteVarPosMin && infiniteVarPos <= infiniteVarPosMax) {
+				infiniteVarPos--;
+			} else {
+				infiniteVarPos = infiniteVarPosMax;
+			}
+			infiniteVarDirection = 'backward';
+		}, infiniteSpeed/speed);
+	}
+	
+	function pause() {
+		clearInterval(infiniteVarInterval);
+	}
+
+	function resume() {
+		if(infiniteAuto == true) {
+			if(infiniteVarDirection == 'forward') {
+				infiniteForward(1);		
+			} else {
+				infiniteBackward(1);
+			}
+		}
+	}
+
+
+
+
+/*
 
 	function xc1Slider() {
 		var xc1Sliders = new Array();
@@ -107,66 +189,13 @@
 			infiniteForward(1);
 		}
 			
-		// Set the slider as loaded
-		infiniteSlider.data('loaded', 'true');
-		
-		// Functions
-		function infiniteForward(speed) {
-			clearInterval(infiniteVarInterval);
-			infiniteVarInterval = setInterval(function() {
-				if(!infiniteVarIE) {
-					infiniteSlider.attr('style', 'width: ' + infiniteVarWidth*6 + 'px; -moz-transform: translate3d(-' + infiniteVarPos +'px, 0, 0); -webkit-transform:translate3d(-' + infiniteVarPos +'px,0,0); transform:translate3d(-' + infiniteVarPos +'px,0,0);');
-				} else {
-					infiniteSlider.css({'left' : '-' + infiniteVarPos + 'px'});
-				}
 				
-				if (infiniteVarPos >= infiniteVarPosMin && infiniteVarPos <= infiniteVarPosMax) {
-					infiniteVarPos++;
-				} else {
-					infiniteVarPos = infiniteVarPosMin;
-				}
-				infiniteVarDirection = 'forward';
-			}, infiniteSpeed/speed);
-		}
-	
-		function infiniteBackward(speed) {
-			clearInterval(infiniteVarInterval);
-			infiniteVarInterval = setInterval(function() {
-				if(!infiniteVarIE) {
-					infiniteSlider.attr('style', 'width: ' + infiniteVarWidth*6 + 'px; -moz-transform: translate3d(-' + infiniteVarPos +'px, 0, 0); -webkit-transform:translate3d(-' + infiniteVarPos +'px,0,0); transform:translate3d(-' + infiniteVarPos +'px,0,0);');
-				} else {
-					infiniteSlider.css({'left' : '-' + infiniteVarPos + 'px'});
-				}
-				
-				if (infiniteVarPos >= infiniteVarPosMin && infiniteVarPos <= infiniteVarPosMax) {
-					infiniteVarPos--;
-				} else {
-					infiniteVarPos = infiniteVarPosMax;
-				}
-				infiniteVarDirection = 'backward';
-			}, infiniteSpeed/speed);
-		}
-		
-		function infinitePause() {
-			clearInterval(infiniteVarInterval);
-		}
-	
-		function infiniteResume() {
-			if(infiniteAuto == true) {
-				if(infiniteVarDirection == 'forward') {
-					infiniteForward(1);		
-				} else {
-					infiniteBackward(1);
-				}
-			}
-		}
-		
 	
 	}
 
 	
 	
-	
+	*/
 	
 	
 	
@@ -284,13 +313,51 @@
 	            gotoPage(page);
 	        });
 	    });  
-	};
+	}
+  
+  //xc1Slider: Defaults
+  $.xc1Slider.defaults = {
+    /*
+	animation: "fade",              //String: Select your animation type, "fade" or "slide"
+    slideDirection: "horizontal",   //String: Select the sliding direction, "horizontal" or "vertical"
+    slideshow: true,                //Boolean: Animate slider automatically
+    slideshowSpeed: 7000,           //Integer: Set the speed of the slideshow cycling, in milliseconds
+    animationDuration: 600,         //Integer: Set the speed of animations, in milliseconds
+    directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
+    controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+    keyboardNav: true,              //Boolean: Allow slider navigating via keyboard left/right keys
+    mousewheel: false,              //Boolean: Allow slider navigating via mousewheel
+    prevText: "Previous",           //String: Set the text for the "previous" directionNav item
+    nextText: "Next",               //String: Set the text for the "next" directionNav item
+    pausePlay: false,               //Boolean: Create pause/play dynamic element
+    pauseText: 'Pause',             //String: Set the text for the "pause" pausePlay item
+    playText: 'Play',               //String: Set the text for the "play" pausePlay item
+    randomize: false,               //Boolean: Randomize slide order
+    slideToStart: 0,                //Integer: The slide that the slider should start on. Array notation (0 = first slide)
+    animationLoop: true,            //Boolean: Should the animation loop? If false, directionNav will received "disable" classes at either end
+    pauseOnAction: true,            //Boolean: Pause the slideshow when interacting with control elements, highly recommended.
+    pauseOnHover: false,            //Boolean: Pause the slideshow when hovering over slider, then resume when no longer hovering
+    controlsContainer: "",          //Selector: Declare which container the navigation elements should be appended too. Default container is the flexSlider element. Example use would be ".flexslider-container", "#container", etc. If the given element is not found, the default action will be taken.
+    manualControls: "",             //Selector: Declare custom control navigation. Example would be ".flex-control-nav li" or "#tabs-nav li img", etc. The number of elements in your controlNav should match the number of slides/tabs.
+    start: function(){},            //Callback: function(slider) - Fires when the slider loads the first slide
+    before: function(){},           //Callback: function(slider) - Fires asynchronously with each slider animation
+    after: function(){},            //Callback: function(slider) - Fires after each slider animation completes
+    end: function(){}               //Callback: function(slider) - Fires when the slider reaches the last slide (asynchronous)
+    */
+  }
+  
+  //xc1Slider: Plugin
+  $.fn.xc1Slider = function(options) {
+    return this.each(function() {
+      if ($(this).find('ul > li').length != 1 || $(this).data('loaded') != true) {
+        new $.xc1Slider($(this), options);
+      } else {
+	  	$(this).find('ul > li').fadeIn();
+      }
+    });
+  }  
+  
 })(jQuery);
-
-$(window).load(function() {
-	xc1Slider();	
-});
-
 
 
 
